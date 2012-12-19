1e766898f7376fe7a41d299fa8e1ad2f2cc16f88
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2012-12-19 17:51:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psychopad::Application.config.secret_token = '58d516353d106b5d3e3e37e7c4e66513ef1597d6ec0a23f71a616edf0088259c9643fb5ba7f880ddb38f015083bbcacdc221699195ccfe7f0f6a97154b4e692b'
