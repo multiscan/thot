1f0feca35eb7d662c54c1f6efdc68410ea95235a
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2013-05-28 14:54:35 +0200

    Book check-in works from adm/users too

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -7,20 +7,17 @@ class LoansController < ApplicationController
         @loan = Loan.checkout(@user, params[:loan]["inv"]||params[:loan]["item_id"].to_i)
         if @loan && !@loan.new_record?
           format.html { redirect_to @user, notice: 'The item you tried to check out is already in your list.' }
-          format.json { render json: @loan }
           format.js { render :js => "thot.alert('notice', 'Item #{@loan.inv} is already in your list.');"}
         elsif @loan && @loan.save
           format.html { redirect_to @user, notice: 'Item was successfully checked out' }
-          format.json { render json: @loan }
           format.js
         else
           format.html { redirect_to @user, error: "An error occurred while checking out the requested item" }
-          format.json { render json: @loan.errors, status: :unprocessable_entity }
           format.js
         end
       else
         format.html { redirect_back_or_root(error: "In ordero to be able to check in/out books, please identify yourself by scanning your Nebis code first.")}
-        format.json { render json: @loan.errors, status: :unprocessable_entity }
+        format.js { render :js => "thot.alert('notice', 'In ordero to be able to check in/out books, please identify yourself by scanning your Nebis code first.');"}
       end
     end
   end
@@ -31,19 +28,16 @@ class LoansController < ApplicationController
     @loan = Loan.find(params[:id])
     respond_to do |format|
       @user = nebis_user
-      if @user.id == @loan.user_id
+      if @user && @user.id == @loan.user_id || admin_signed_in? && can?(:update, @loan.user)
         if @loan.checkin
-          format.html { redirect_to users_url }
-          format.json { head :no_content }
-          format.js   # { render :js => "$('#loan_li_#{@loan.id}').fadeOut();" }
+          format.html { redirect_to @loan.user }
+          format.js
         else
-          format.html { redirect_to users_url, :error=>"Error while returning item #{@loan.item.id}" }
-          format.json { head :no_content }
+          format.html { redirect_to @loan.user, :error=>"Error while returning item #{@loan.item.id}" }
           format.js { render :js => 'thot.alert("error", "Error while returning item #{@loan.item.id}");', :status => :internal_server_error }
         end
       else
         format.html { redirect_to users_url, :error=>"Error while returning item #{@loan.item.id}" }
-        format.json { head :no_content }
         format.js { render :js => 'thot.alert("error", "Forbidden.");', :status => :forbidden }
       end
     end
